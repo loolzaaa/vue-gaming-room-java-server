44fead2c89a86d2bc1e2f3c99bcdc85e12560fbf
Korsakov A.N. (20211) <loolzaaa@gmail.com>
2024-09-09 16:17:54 +0500

    Bugfix in GameController

--- java-server/src/main/java/ru/loolzaaa/games/vuegamingroomjavaserver/javaserver/controller/GameController.java
+++ java-server/src/main/java/ru/loolzaaa/games/vuegamingroomjavaserver/javaserver/controller/GameController.java
@@ -1,6 +1,7 @@
 package ru.loolzaaa.games.vuegamingroomjavaserver.javaserver.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class GameController {
         return new GameInfo(gameService.getGameName());
     }
 
+    @Getter
     @AllArgsConstructor
     public static class GameInfo {
         String gameName;
